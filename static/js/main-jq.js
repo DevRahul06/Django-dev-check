eaf05cb826c9f191133d14e85c2df3a81935f630
Rahul <rahulchavda6000@gmail.com>
2025-04-05 13:23:34 +0530

    Add All slider in home page and Banner Image

--- static/js/main-jq.js
+++ static/js/main-jq.js
@@ -16,4 +16,79 @@ $(document).ready(function () {
   $(".home-prev").click(function () {
     $(".home-slider").trigger("prev.owl.carousel");
   });
+
+  // Service Card Section Start
+  $(".service-slider").owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    autoHeight: false,
+    responsive: {
+      0: {
+        items: 1, // Show 1 item on small screens
+      },
+      767: {
+        items: 1.5, // Show 2 items on tablets
+      },
+
+      1000: {
+        items: 2, // Show 3 items on desktops
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+  // Custom navigation button events
+  $(".service-next").click(function () {
+    $(".service-slider").trigger("next.owl.carousel");
+  });
+
+  $(".service-prev").click(function () {
+    $(".service-slider").trigger("prev.owl.carousel");
+  });
+
+
+  // client-slider Section
+
+  $(".client-slider").owlCarousel({
+    items: 4,
+    margin: 10,
+    loop: true,
+    nav: false,
+    center: true, 
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    autoHeight: false,
+    responsive: {
+      0: {
+        items: 1, // Show 1 item on small screens
+      },
+      767: {
+        items: 1.5, // Show 2 items on tablets
+      },
+
+      1000: {
+        items: 2, // Show 3 items on desktops
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+  // Custom navigation button events
+  $(".client-next").click(function () {
+    $(".client-slider").trigger("next.owl.carousel");
+  });
+
+  $(".client-prev").click(function () {
+    $(".client-slider").trigger("prev.owl.carousel");
+  });
 });
